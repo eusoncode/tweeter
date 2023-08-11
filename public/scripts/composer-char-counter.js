3bff6ce075bc1afb6166cd0485253999aa681191
eusoncode <git@github.com:eusoncode/Kata.git>
2023-08-10 22:42:00 -0600

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+  $('#tweet-text').on('input', function() {
+    const charCount = $(this).val().length; // Get the character count
+    const $counter = $(this).closest('.new-tweet').find('.counter'); // Get the counter element
+    $counter.text(140 - charCount); // Update the counter text
+
+    //   // Add or remove the "invalid" class based on the character left
+    if (charCount > 140) {
+      $counter.addClass('invalid'); // Add the class for red color
+    } else {
+      $counter.removeClass('invalid'); // Remove the class
+    }
+
+  });
+
+});
